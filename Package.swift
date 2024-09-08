9bcf31aa76c77be6d24d5d8fb95a194da1b201e4
Marcin Iwanicki <iwan@icode.pl>
2024-09-08 19:44:30 +0100

    Update SwiftCommons to 0.2.1

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .macOS(.v13),
     ],
     dependencies: [
-        .package(url: "https://github.com/mobileinf/SwiftCommons.git", .upToNextMajor(from: "0.2.0")),
+        .package(url: "https://github.com/mobileinf/SwiftCommons.git", .upToNextMajor(from: "0.2.1")),
         .package(url: "https://github.com/apple/swift-tools-support-core", .upToNextMajor(from: "0.5.2")),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.2.3")),
     ],
