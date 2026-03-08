51a8d57f32ba76225bc50ab548d188b6e493f7fe
Marcin Iwanicki <iwan@icode.pl>
2026-03-08 00:10:29 +0000

    Update ArgumentParser to 1.7.0 (#160)
    Test Plan:
    - Ensure all CI checks pass

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/macvmio/SwiftCommons.git", .upToNextMajor(from: "0.3.0")),
         .package(url: "https://github.com/apple/swift-tools-support-core", .upToNextMajor(from: "0.6.1")),
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.5.0")),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMajor(from: "1.7.0")),
     ],
     targets: [
         .executableTarget(
